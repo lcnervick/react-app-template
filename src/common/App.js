57eff735c1f6ecdb04d91fa53db1f9ec0db30176
leif@synapticsoftware.net <leif@synapticsoftware.net>
2023-07-27 13:07:54 -0600

    update directory structure

--- src/common/App.js
+++ src/common/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
-import ROUTES from '../routes/routes';
+import ROUTES from './routes/routes';
 
 export default function App() {
 	return (<>
